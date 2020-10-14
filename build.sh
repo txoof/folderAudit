a994717561b49f4e211d224c7da1b32d7de59ac2
txoof <aaron.ciuffo@gmail.com>
2020-10-14 15:14:30 +0200

    update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 APPNAME="folderAudit"
 DATESTR=`date +%F_%H.%M`
 
-pipenv run  pyinstaller --clean *.spec
+pipenv run pyinstaller --clean *.spec
+
+pushd ./dist
+tar cvzf ../folderAudit_latest.tgz .
+popd
+
 #mv /Volumes/GoogleDrive/Team\ Drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/*.zip /Volumes/GoogleDrive/Team\ Drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/Old\ Versions
 #zip -j /Volumes/GoogleDrive/Team\ Drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/portfolioCreator_$DATESTR.zip ./dist/portfolioCreator
 
