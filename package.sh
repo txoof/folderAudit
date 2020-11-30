7c4f873287a68135fafe620eebcbcef358db63da
txoof <aaron.ciuffo@gmail.com>
2020-11-30 15:08:19 +0100

    update to use codesign

--- package.sh
+++ package.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-pushd ./dist
+#pushd ./dist
 
-tar cvzf ../folderAudit_latest.tgz .
+#tar cvzf ../folderAudit_latest.tgz .
 
-popd
+#popd
 
-git commit -m "refresh tgz" ./folderAudit_latest.tgz
+#git commit -m "refresh tgz" ./folderAudit_latest.tgz
+
+~/bin/develtools/pycodesign.py folderAudit_codesign.ini
+if [ $? -eq 0 ] 
+then
+  git commit -m "refresh package"  folderAudit.pkg
+  git push
+fi
+  echo codesign failed
+
