7f3cb765fd169fbc846d0afc1b44452d428b0b0b
txoof <aaron.ciuffo@gmail.com>
2020-11-30 14:45:45 +0100

    remove packaging

--- build.sh
+++ build.sh
@@ -2,13 +2,4 @@
 
 pipenv run pyinstaller --clean *.spec
 
-codesign -s aaron.ciuffo@gmail.com ./dist/folderAudit
-
-pushd ./dist
-
-tar cvzf ../folderAudit_latest.tgz .
-popd
-
-#mv /Volumes/GoogleDrive/Team\ Drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/*.zip /Volumes/GoogleDrive/Team\ Drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/Old\ Versions
-#zip -j /Volumes/GoogleDrive/Team\ Drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/portfolioCreator_$DATESTR.zip ./dist/portfolioCreator
 
